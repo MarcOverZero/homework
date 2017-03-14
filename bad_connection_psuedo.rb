22ce53b8a7af8a7f6658f14a5025f7c6578c5302
marcoverzero <marcoverzero@gmail.com>
2017-03-14 09:24:22 -0600

    demonstrates file edit

--- bad_connection_psuedo.rb
+++ bad_connection_psuedo.rb
@@ -4,3 +4,5 @@ If the user input is entered in lower-case and has a question mark, bad_connecti
 If the user input is entered in upper-case and has a question mark, bad_connection returns "NO, THIS IS NOT A PET STORE."
 If the user input is "GOODBYE!" and it is the first time they have said it, bad_connection returns "ANYTHING ELSE I CAN HELP WITH?"
 If the user input is "GOODBYE!" and it is the second time they say it, bad_connection returns "THANK YOU FOR CALLING!"
+
+much changes so git
