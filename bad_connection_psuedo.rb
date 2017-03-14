8b85c66a46c9a033eb495c87c15c94163be286e3
marcoverzero <marcoverzero@gmail.com>
2017-03-14 09:09:51 -0600

    initial commit

---
+++ bad_connection_psuedo.rb
@@ -0,0 +1,6 @@
+start bad_connection
+If the user input is nothing before enter, bad_connection returns "HELLO?!"
+If the user input is entered in lower-case and has a question mark, bad_connection returns "I AM HAVING A HARD TIME HEARING YOU."
+If the user input is entered in upper-case and has a question mark, bad_connection returns "NO, THIS IS NOT A PET STORE."
+If the user input is "GOODBYE!" and it is the first time they have said it, bad_connection returns "ANYTHING ELSE I CAN HELP WITH?"
+If the user input is "GOODBYE!" and it is the second time they say it, bad_connection returns "THANK YOU FOR CALLING!"
